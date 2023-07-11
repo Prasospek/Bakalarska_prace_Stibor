c3955fa5c928036b84cfa749f24d4a44b7bd2759
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-07-11 03:38:02 +0200

    Navbar

--- frontend/src/scenes/navbar/index.js
+++ frontend/src/scenes/navbar/index.js
@@ -1,22 +1,58 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    IconButton,
+    Icon,
+    Tooltip,
+    Box,
+} from "@mui/material";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 import { setMode } from "../../state";
 import { useDispatch, useSelector } from "react-redux";
 import { useTheme } from "@emotion/react";
+import HelpCenterIcon from "@mui/icons-material/HelpCenter";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
     const dispatch = useDispatch();
     const theme = useTheme();
     const dark = theme.palette.neutral.dark;
+    const navigate = useNavigate();
+
+    const handleNavigate = () => {
+        navigate("http://localhost:8000/help");
+    };
 
     return (
-        <AppBar position="static">
+        <AppBar
+            position="static"
+            sx={{ backgroundColor: "rgba(38, 173, 217, 0.5)" }}
+        >
             <Toolbar>
+                <Typography
+                    fontWeight="bold"
+                    fontSize="clamp(1rem, 2rem, 2.25rem)"
+                    onClick={() => navigate("/home")}
+                    sx={{
+                        "&:hover": {
+                            cursor: "pointer",
+                        },
+                    }}
+                >
+                    ShareTaxMax
+                </Typography>
+
                 <Typography
                     variant="body1"
                     component="div"
-                    sx={{ flexGrow: 1 }}
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        marginLeft: "auto",
+                    }}
                 >
                     <Typography
                         variant="body1"
@@ -37,18 +73,30 @@ function Navbar() {
                         component="span"
                         sx={{ marginRight: "16px" }}
                     >
-                        Contact
+                        Časté dotazy
                     </Typography>
                 </Typography>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Your Logo
-                </Typography>
+
+                <Tooltip
+                    sx={{ marginLeft: 15 }}
+                    title={
+                        <Typography variant="body1" sx={{ fontSize: 12 }}>
+                            Potřebujete pomoc nebo máte otázku ?
+                        </Typography>
+                    }
+                    placement="bottom"
+                >
+                    <IconButton onClick={handleNavigate}>
+                        <HelpCenterIcon sx={{ fontSize: 25 }} />
+                    </IconButton>
+                </Tooltip>
+
                 <IconButton
                     onClick={() => dispatch(setMode())}
-                    sx={{ fontSize: "25px" }}
+                    sx={{ fontSize: "25px", marginLeft: "22px" }}
                 >
                     {theme.palette.mode === "dark" ? (
-                        <Brightness4Icon sx={{ fontSize: "25px" }} />
+                        <Brightness7Icon sx={{ fontSize: "25px" }} />
                     ) : (
                         <Brightness4Icon
                             sx={{ color: dark, fontSize: "25px" }}
